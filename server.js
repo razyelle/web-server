997d7db1df771f58f438072207019ce70811ea2d
Moncef Benboubakeur <moncef.benboubakeur@cz.ibm.com>
2017-04-09 23:23:59 +0200

    Add POST for todos

--- server.js
+++ server.js
@@ -1,28 +1,23 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: 'Meet Hamdi for lunch',
-    completed: false
-}, {
-    id: 2,
-    description: 'Go to market',
-    completed: false
-}, {
-    id: 3,
-    description: 'Take a coffee',
-    completed: true    
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req,res){
     res.send('Todo Api Root');
 });
 
+// GET /todos
 app.get('/todos',function(req,res){
     res.json(todos);
 });
 
+//GET /todos/:id
 app.get('/todos/:id',function(req,res){
     var todoId = parseInt(req.params.id, 10);
     var matchedTodo;
@@ -38,7 +33,16 @@ app.get('/todos/:id',function(req,res){
     } else {
         res.status(404).send();
     }
+});
 
+//POST /todos
+app.post('/todos', function (req,res) {
+    var body = req.body;
+
+    body.id = todoNextId;
+    todoNextId++;
+    todos.push(body);
+    res.json(body);
 });
 
 app.listen(PORT, function(){
