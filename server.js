8243cffd8a646a0036dd6adf33a216a8d5a67791
Moncef Benboubakeur <moncef.benboubakeur@cz.ibm.com>
2017-04-17 20:19:42 +0200

    Update PUT /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -129,27 +129,30 @@ app.delete('/todos/:id', function(req,res){
 //PUT /todos/:id
 app.put('/todos/:id', function (req,res) {
     var body = _.pick(req.body, "description", "completed");
-    var validAttributes = {};
+    var attributes = {};
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
 
-    if (!matchedTodo) {
-       res.status(404).json({"error":"no todo item found with that id"}); 
-    }
-
-    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed) ) {
-        validAttributes.completed = body.completed;
-    } else if (body.hasOwnProperty('completed')){
-        res.status(400).json({"error":"bad request (completed field) "});
+    if (body.hasOwnProperty('completed') ) {
+        attributes.completed = body.completed;
     } 
 
-    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0 ) {
-        validAttributes.description = body.description;
-    } else if (body.hasOwnProperty('description')){
-        res.status(400).json({"error":"bad request (description field)"});
+    if (body.hasOwnProperty('description')) {
+        attributes.description = body.description.trim();
     } 
 
-    res.json(_.extend(matchedTodo, validAttributes));
+    db.todo.findById(todoId).then(function (todo) {
+        if (todo) {
+            return todo.update(attributes).then(function (todo) {
+                res.json(todo.toJSON());
+            }, function (e) {
+                res.status(400).json(e);
+            });
+        } else {
+            res.status(404).send();
+        }
+    }, function () {
+        res.status(500).send();
+    });
 });
 
 db.sequelize.sync({
